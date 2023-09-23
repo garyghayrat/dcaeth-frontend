95d14c60abf151c9c65fd134f5e2a7a9b24e55a7
garyghayrat <gary.ghayrat@pm.me>
2023-09-23 18:46:25 -0400

    Add shadcn UI skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,68 @@
 import { Inter } from "next/font/google";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { usePrepareContractWrite, useContractWrite } from "wagmi";
 
 const inter = Inter({ subsets: ["latin"] });
+const DCADailyAddress = "0x0";
 
 export default function Home() {
+  const { config } = usePrepareContractWrite({
+    address: DCADailyAddress,
+    abi: [
+      {
+        name: "mint",
+        type: "function",
+        stateMutability: "nonpayable",
+        inputs: [],
+        outputs: [],
+      },
+    ],
+    functionName: "mint",
+  });
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <ConnectButton />
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <div>hello</div>
+      <div>
+        <ConnectButton />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Sign Up</CardTitle>
+            <CardDescription>Decide recurring buy amount</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <p>Recurring Amount</p>
+                <Input />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <p>Total Amount</p>
+                <Input />
+              </div>
+            </div>
+          </CardContent>
+
+          <CardFooter>
+            <Button>Sign Up</Button>
+          </CardFooter>
+        </Card>
+      </div>
+      <div />
     </main>
   );
 }
